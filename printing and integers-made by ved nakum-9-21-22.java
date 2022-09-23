4d39212cf4d465b1223a2967ec8986c95f4f3f3e
VedNakum <113805164+VedNakum@users.noreply.github.com>
2022-09-22 21:31:22 -0400

    CODING EXTRAS
    may have weird tests. LEARNING JAVAVAVAV

---
+++ printing and integers-made by ved nakum-9-21-22.java
@@ -0,0 +1,7 @@
+public class Main {
+    int teamNumber = 16883;
+    //creates a integer than you can put numbers (like the team number) 
+    System.out.println(teamNumber + "is better than baker dynamics! ");
+    //prints out the integer + the normal print of "is better than baker dynamics"
+    }
+
