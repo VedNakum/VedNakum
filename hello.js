5c30f984d91cf0486494488949c8131622ea4e78
VedNakum <113805164+VedNakum@users.noreply.github.com>
2024-03-16 19:46:03 -0400

    hello

---
+++ hello.js
@@ -0,0 +1,25 @@
+function animateValue(id, start, end, duration) {
+  if (start === end) return;
+  var range = end - start;
+  var current = start;
+  var increment = end > start? 1 : -1;
+  var stepTime = Math.abs(Math.floor(duration / range));
+  var obj = document.getElementById(id);
+  var timer = setInterval(function() {
+      current += increment;
+      obj.innerHTML = current;
+      if (current == end) {
+          clearInterval(timer);
+      }
+  }, stepTime);
+}
+
+let scrolled = false
+window.addEventListener('scroll', function(){
+  if(window.scrollY > 100 && !scrolled) {
+    scrolled = true;
+    console.log('new section')
+    document.getElementById('target').style.display="block"
+    animateValue("value", 0, 2000, 1);
+  }
+})
